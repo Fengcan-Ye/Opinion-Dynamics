9b8f34b5f2c417c06fbf159f86b7ee38cefbaa45
Fengcan Ye <yfcfloyd@pku.edu.cn>
2023-07-27 22:37:41 +0800

    Wrapped simulation code in Society class

--- opinion_dynamics.py
+++ opinion_dynamics.py
@@ -1,15 +1,64 @@
 import networkx as nx
 import numpy as np
+from censor import *
 
-import copy
 
-def initialize_opinions(network : nx.Graph):
-    for i in range(network.number_of_nodes()):
-        network.nodes[i]['intrinsic opinion'] = [np.random.uniform(-1, 1)]
-        network.nodes[i]['extrinsic opinion'] = copy.copy(network.nodes[i]['intrinsic opinion'])
+class Society:
+    def __init__(self, network : nx.Graph, 
+                 K : float, 
+                 alpha : float, 
+                 beta : float,
+                 dt : float):
+        self.network = network 
+        self.intrinsic_opinions, self.extrinsic_opinions = initialize_opinions(network.number_of_nodes())
+        self.K = K 
+        self.alpha = alpha 
+        self.beta = beta 
+        self.dt = dt
+        self.coupled = False
+    
+    def current_opinions(self, key : str):
+        if key.startswith('i'):
+            return self.intrinsic_opinions[:, -1]
+        elif key.startswith('e'):
+            return self.extrinsic_opinions[:, -1]
+        else:
+            raise ValueError
+    
+    def couple(self, censor : Censor):
+        self.censor = censor
+        self.censor.couple(self)
+        self.coupled = True
+    
+    def update(self):
+        intrinsics = self.current_opinions(key='intrinsic opinion')
+        extrinsics = self.current_opinions(key='extrinsic opinion')
 
-def current_opinions(network : nx.Graph, key = 'intrinsic opinion'):
-    return np.array([network.nodes[i][key][-1] for i in range(network.number_of_nodes())])
+        # P(i is influenced by j) ~ |I_i - E_j|^{-beta}  where j is a neighbor of i
+        p_unnormalized = np.abs(intrinsics.reshape(-1, 1) - extrinsics) ** (-self.beta) * nx.adjacency_matrix(self.network)
+        p_unnormalized = np.nan_to_num(p_unnormalized, nan=0, posinf=0, neginf=0)
+        p = p_unnormalized / np.sum(p_unnormalized, axis=1).reshape(-1, 1)
+
+        us = np.random.uniform(0, 1, size = p.shape)
+        A = p > us
+    
+        intrinsics = intrinsics + \
+                     RK4(intrinsics, 
+                         lambda I: dxdt(I, extrinsics, A, self.K, self.alpha), 
+                         self.dt)
+        self.intrinsic_opinions = np.hstack([self.intrinsic_opinions, intrinsics.reshape(-1, 1)])
+        self.extrinsic_opinions = np.hstack([self.extrinsic_opinions, intrinsics.reshape(-1, 1)])
+
+        if self.coupled:
+            self.censor.update()
+    
+    def trajectories(self):
+        return self.intrinsic_opinions, self.extrinsic_opinions
+
+
+def initialize_opinions(n):
+    intrinsic_opinions = np.random.uniform(low=-1, high=1, size=(n, 1))
+    return intrinsic_opinions, intrinsic_opinions.copy()
 
 def RK4(x : np.array, f, h):
     # 4th order Runge-Kutta method
@@ -25,29 +74,4 @@ def RK4(x : np.array, f, h):
 def dxdt(I : np.array, E : np.array, A : np.array, K : float, alpha : float):
     # x: a column vector (N, 1)
     return -I + K * (A  @ np.tanh(alpha * E))
-
-
-def update_opinions(network : nx.Graph, 
-                    K : float, 
-                    alpha : float, 
-                    beta : float,
-                    discount_factor : float,
-                    dt : float):
-    intrinsics = current_opinions(network, 'intrinsic opinion')
-    extrinsics = current_opinions(network, 'extrinsic opinion')
-
-    # P(i is influenced by j) ~ |I_i - E_j|^{-beta}  where j is a neighbor of i
-    p_unnormalized = np.abs(intrinsics.reshape(-1, 1) - extrinsics) ** (-beta) * nx.adjacency_matrix(network)
-    p_unnormalized = np.nan_to_num(p_unnormalized, nan=0, posinf=0, neginf=0)
-    p = p_unnormalized / np.sum(p_unnormalized, axis=1).reshape(-1, 1)
-
-    us = np.random.uniform(0, 1, size = p.shape)
-    A = p > us
-    
-    intrinsics = intrinsics + RK4(intrinsics, lambda I: dxdt(I, extrinsics, A, K, alpha), dt)
-
-    for i in range(network.number_of_nodes()):
-        network.nodes[i]['intrinsic opinion'].append(intrinsics[i])
-        network.nodes[i]['extrinsic opinion'].append(intrinsics[i])
-
 
